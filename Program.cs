1a34c200515ba421a5734d8b019b30d2def3dab8
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-07-20 08:20:00 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace SimpleHotelRoomManagementProject_CSharpProject2_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+
+
+
+        }// End of Main method
+
+
+
+
+    } // End of Program class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} // End of namespace SimpleHotelRoomManagementProject_CSharpProject2_OOP
